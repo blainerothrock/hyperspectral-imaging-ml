6089f79e898e4fba6280185ff8f2ae57119037ea
Zane <zanedenmon2020@u.northwestern.edu>
2020-05-08 10:46:54 -0500

    breaks up transforms into pca and image transform files

---
+++ hyperspec/transforms/image_transform.py
@@ -0,0 +1,44 @@
+from . import TransformException, BaseTransform
+
+import numpy as np
+
+class Preprocessing(BaseTransform):
+    """
+    Args:
+    Raises:
+        TransformException
+    Returns:
+    """
+
+    def __init__(
+            self,
+    source,
+    output,
+    inplace):
+        super().__init__(source, output, inplace)
+
+    def padWithZeros(self, margin=2):
+        newX = np.zeros((self.shape[0] + 2 * margin, self.shape[1] + 2 * margin, self.shape[2]))
+        x_offset = margin
+        y_offset = margin
+        newX[x_offset:self.shape[0] + x_offset, y_offset:self.shape[1] + y_offset, :] = self
+        return newX
+
+    def createImageCubes(self, y, windowSize=5, removeZeroLabels = True):
+        margin = int((windowSize - 1) / 2)
+        zeroPaddedX = self.padWithZeros(self, margin=margin)
+        # split patches
+        patchesData = np.zeros((self.shape[0] * self.shape[1], windowSize, windowSize, self.shape[2]))
+        patchesLabels = np.zeros((self.shape[0] * self.shape[1]))
+        patchIndex = 0
+        for r in range(margin, zeroPaddedX.shape[0] - margin):
+            for c in range(margin, zeroPaddedX.shape[1] - margin):
+                patch = zeroPaddedX[r - margin:r + margin + 1, c - margin:c + margin + 1]
+                patchesData[patchIndex, :, :, :] = patch
+                patchesLabels[patchIndex] = y[r-margin, c-margin]
+                patchIndex = patchIndex + 1
+        if removeZeroLabels:
+            patchesData = patchesData[patchesLabels>0,:,:,:]
+            patchesLabels = patchesLabels[patchesLabels>0]
+            patchesLabels -= 1
+        return patchesData, patchesLabels
---
+++ hyperspec/transforms/pca.py
@@ -0,0 +1,26 @@
+from . import TransformException, BaseTransform
+
+import numpy as np
+from sklearn.decomposition import PCA
+
+class Preprocessing(BaseTransform):
+    """
+    Args:
+    Raises:
+        TransformException
+    Returns:
+    """
+
+    def __init__(
+            self,
+    source,
+    output,
+    inplace):
+        super().__init__(source, output, inplace)
+
+    def applyPCA(self, numComponents=75):
+        newX = np.reshape(self, (-1, self.shape[2]))
+        pca = PCA(n_components=numComponents, whiten=True)
+        newX = pca.fit_transform(newX)
+        newX = np.reshape(newX, (self.shape[0], self.shape[1], numComponents))
+        return newX, pca

afb1b58e224f8268d5f809b02a0ec230a52a71c4
Blaine Rothrock <blaine.p.rothrock@gmail.com>
2020-05-23 14:51:09 -0500

    ignite, gin, train/test split

--- train.py
+++ train.py
@@ -1,41 +1,73 @@
-import torch
+import torch, gin
 from torch.utils.data import DataLoader
-from hyperspec.datasets import IndianPineDataset
+from torch.utils.tensorboard import SummaryWriter
+from hyperspec.datasets import indian_pine_split
 from hyperspec.model import HybridSN
-from torchsummary import summary
 import numpy as np
+from ignite.engine import create_supervised_evaluator, Events
+from ignite.metrics import Accuracy, Loss
+from ignite.handlers import ModelCheckpoint
+from ignite_utils import create_engine, prepare_batch
+from torchsummary import summary
 
-ds = IndianPineDataset()
-dl = DataLoader(ds, batch_size=256, shuffle=True)
+@gin.configurable()
+def train(batch_size, learning_rate, weight_decay, max_epochs):
+    train_ds, test_ds = indian_pine_split()
 
-device = torch.device('cuda')
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    test_dl = DataLoader(test_ds, batch_size=batch_size, shuffle=True)
+
+    device = torch.device('cuda')
+
+    model = HybridSN()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    trainer = create_engine(model, optimizer, loss_fn, device)
+    eval = create_supervised_evaluator(
+        model,
+        metrics={'accuracy': Accuracy(), 'nll': Loss(loss_fn)},
+        prepare_batch=prepare_batch,
+        device=device
+    )
+
+    summary(model, next(iter(train_dl))[0].unsqueeze(1).shape[1:])
+
+    checkpoint_handler = ModelCheckpoint('./models', 'hybridsn', n_saved=1, create_dir=True, require_empty=False)
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpoint_handler, {'model': model})
+
+    writer = SummaryWriter()
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def report_iter_loss(trainer):
+        writer.add_scalar('iter/train_loss', trainer.state.output, trainer.state.iteration)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def report_training_epoch(trainer):
+        print('\nEpoch {}'.format(trainer.state.epoch))
+        eval.run(train_dl)
+        metrics = eval.state.metrics
+        print('  - training:')
+        print('    - loss: {:.2f}'.format(metrics['nll']))
+        print('    - acc:  {:.2f}'.format(metrics['accuracy']))
+
+        writer.add_scalar('loss/train', metrics['nll'], trainer.state.epoch)
+        writer.add_scalar('accuracy/train', metrics['accuracy'], trainer.state.epoch)
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def report_testing_epoch(trainer):
+        eval.run(test_dl)
+        metrics = eval.state.metrics
+        print('  - testing:')
+        print('    - loss: {:.2f}'.format(metrics['nll']))
+        print('    - acc:  {:.2f}'.format(metrics['accuracy']))
+
+        writer.add_scalar('loss/test', metrics['nll'], trainer.state.epoch)
+        writer.add_scalar('accuracy/test', metrics['accuracy'], trainer.state.epoch)
+
+    trainer.run(train_dl, max_epochs=max_epochs)
 
 
-model = HybridSN()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-06)
-loss_fn = torch.nn.NLLLoss()
-
-# summary(model, (1,25,25,30))
-
-model.to(device)
-model.train()
-
-for epoch in range(100):
-    print('epoch {}'.format(epoch+1))
-    running_loss = []
-    for batch in dl:
-        data, label = batch
-        n = data.numpy()
-        data = data.unsqueeze(1).float().to(device)
-        label = label.to(torch.int64).to(device)
-
-        optimizer.zero_grad()
-
-        output = model(data)
-        _loss = loss_fn(output, label)
-        _loss.backward()
-        optimizer.step()
-        running_loss.append(_loss.cpu().detach().item())
-
-    print('  - {}: Loss: {}'.format(epoch, np.mean(running_loss)))
-    running_loss = []
+if __name__ == '__main__':
+    gin.parse_config_file('config.gin')
+    train()

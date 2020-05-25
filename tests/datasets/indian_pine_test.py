3832c465809b5f206dc2025c4faf1e63ced3e5a6
Zane <zanedenmon2020@u.northwestern.edu>
2020-05-25 16:42:27 -0500

    adds test for indian pines train/test split

--- tests/datasets/indian_pine_test.py
+++ tests/datasets/indian_pine_test.py
@@ -1,4 +1,5 @@
 import pytest
+from hyperspec.datasets import indian_pine_split
 from hyperspec.datasets import IndianPineDataset
 from torch.utils.data import DataLoader
 import torch
@@ -21,6 +22,30 @@ class TestIndianPineDataset:
         assert X.shape == (batch_size, window_size, window_size, K)
         assert isinstance(y, torch.Tensor), 'y should be a Tensor'
         assert len(y) == batch_size
+
+    def test_split(self):
+        force_download = False
+        K = 30
+        window_size = 25
+        train_split=0.7
+
+        train_ds, test_ds = indian_pine_split(force_download=force_download, K=K, window_size=window_size, train_split=train_split)
+
+        assert train_ds.img.data.shape[0] == train_ds.labels.data.shape[0]
+        assert train_ds.img.data.shape[1] == train_ds.img.data.shape[2] == window_size
+        assert train_ds.img.data.shape[3] == K
+
+        assert test_ds.img.data.shape[0] == test_ds.labels.data.shape[0]
+        assert test_ds.img.data.shape[1] == test_ds.img.data.shape[2] == window_size
+        assert test_ds.img.data.shape[3] == K
+
+        assert train_ds.img.data.shape[0] > test_ds.img.data.shape[0]
+        assert int((train_ds.img.data.shape[0] + test_ds.img.data.shape[0]) * train_split) == train_ds.img.data.shape[0]
+        assert int((train_ds.img.data.shape[0] + test_ds.img.data.shape[0]) * (1-train_split)) + 1 == test_ds.img.data.shape[0]
+
+
+
+
 
 
 

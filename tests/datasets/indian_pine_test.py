0cb292f70e8c52d93a2e0cf130107061802da412
Blaine Rothrock <blaine.p.rothrock@gmail.com>
2020-05-31 20:36:29 -0500

    shuffle sampler indian pines dataset

--- tests/datasets/indian_pine_test.py
+++ tests/datasets/indian_pine_test.py
@@ -7,7 +7,6 @@ import torch
 
 class TestIndianPineDataset:
 
-    # TODO: placeholder tests
     def test_initialization(self):
         K = 12
         window_size = 13
@@ -24,24 +23,13 @@ class TestIndianPineDataset:
         assert len(y) == batch_size
 
     def test_split(self):
-        force_download = False
-        K = 30
-        window_size = 25
-        train_split=0.7
+        train_dl, test_dl = indian_pine_split(batch_size=1)
 
-        train_ds, test_ds = indian_pine_split(force_download=force_download, K=K, window_size=window_size, train_split=train_split)
+        train_indices = train_dl.batch_sampler.sampler.indices
+        test_indices = test_dl.batch_sampler.sampler.indices
 
-        assert train_ds.img.data.shape[0] == train_ds.labels.data.shape[0]
-        assert train_ds.img.data.shape[1] == train_ds.img.data.shape[2] == window_size
-        assert train_ds.img.data.shape[3] == K
-
-        assert test_ds.img.data.shape[0] == test_ds.labels.data.shape[0]
-        assert test_ds.img.data.shape[1] == test_ds.img.data.shape[2] == window_size
-        assert test_ds.img.data.shape[3] == K
-
-        assert train_ds.img.data.shape[0] > test_ds.img.data.shape[0]
-        assert int((train_ds.img.data.shape[0] + test_ds.img.data.shape[0]) * train_split) == train_ds.img.data.shape[0]
-        assert int((train_ds.img.data.shape[0] + test_ds.img.data.shape[0]) * (1-train_split)) + 1 == test_ds.img.data.shape[0]
+        for idx in train_indices:
+            assert idx not in test_indices, 'no index should overlap'
 
 
 
